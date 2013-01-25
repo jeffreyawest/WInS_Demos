faa4e3c0a5e79ab4fcd51946beeead048f2e4f5b
Jeff West <jeffrey.west@oracle.com>
2013-01-25 08:58:34 -0800

    new

---
+++ spring-examples/spring-jms-example/spring-jms-temp/src/main/java/jms/InvoiceMDB.java
@@ -0,0 +1,21 @@
+package jms;
+
+/**
+ * **************************************************************************
+ * <p/>
+ * This code is provided for example purposes only.  Oracle does not assume
+ * any responsibility or liability for the consequences of using this code.
+ * If you choose to use this code for any reason, including but not limited
+ * to its use as an example you do so at your own risk and without the support
+ * of Oracle.
+ * <p/>
+ * ****************************************************************************
+ * Created with IntelliJ IDEA because its awesome.
+ * User: jeffreyawest
+ * Date: 1/25/13
+ * Time: 8:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InvoiceMDB
+{
+}
