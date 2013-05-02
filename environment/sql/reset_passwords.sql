2483843f55e8093f0da34932430cfeba7bf539b6
Jeff West <jeffrey.west@oracle.com>
2013-05-01 17:42:19 -0700

    split sql into two

--- environment/sql/reset_passwords.sql
+++ environment/sql/reset_passwords.sql
@@ -1,11 +1,3 @@
--- truncates tables otherwise WLS can get confused...
-truncate table weblogic_examples_domain.active drop storage;
-truncate table saf_target_domain.active drop storage;
-truncate table weblogic_examples_domain.jmsstore1wlstore drop storage;
-truncate table weblogic_examples_domain.jmsstore2wlstore drop storage;
-truncate table weblogic_examples_domain.safstore1wlstore drop storage;
-truncate table weblogic_examples_domain.safstore2wlstore drop storage;
-
 ALTER PROFILE DEFAULT LIMIT
 FAILED_LOGIN_ATTEMPTS UNLIMITED
 PASSWORD_LIFE_TIME UNLIMITED;
