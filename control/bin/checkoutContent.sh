ec40172f48c24c1a366041c2cb98c5eca88c4d2f
Jeff West <jeffrey.west@oracle.com>
2013-05-08 11:23:19 -0700

    not needed

--- control/bin/checkoutContent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /labs/content/WInS_labs
-echo "Updating the lab materials"
-svn update
-cd /labs/content/WInS_Demos
-echo "Updating the demo materials"
-svn update
-
-read -p "Checkout complete. Press [Enter] to close the window"
